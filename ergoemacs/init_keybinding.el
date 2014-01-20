fa75fe1ddaebf7e44b5b15789c1e642845db111c
Xah Lee <xah@xahlee.org>
2014-01-20 06:30:00 -0800

    * ergoemacs/init_keybinding.el: trivial comment improvement

--- ergoemacs/init_keybinding.el
+++ ergoemacs/init_keybinding.el
@@ -9,14 +9,14 @@
 
 (defun browser-nav-keys ()
   "Add some browser styled nav keys for Info-mode.
-  The following keys and mouse buttons are added:
+  The following keys are added:
  【Backspace】 for `Info-history-back'
  【Shift+Backspace】 for `Info-history-forward'."
   (progn
     (local-set-key (kbd "<backspace>") 'Info-history-back)
     (local-set-key (kbd "<S-backspace>") 'Info-history-forward)
     )
-  ;; note: Backspace key for previous page isn't support by Firefox on Linux. That key isn't support in Mac's Safari neither until i think 2008 or so.
+  ;; note: on Linux Firefox, you have to turn on Backspace key for previous page. In the preference. That key isn't supported in Mac's Safari neither until i think 2008 or so.
   )
 
 (add-hook 'Info-mode-hook 'browser-nav-keys)
