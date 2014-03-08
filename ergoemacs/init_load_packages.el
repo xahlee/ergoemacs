c187b5ccd9ae081213fcb41fc4bfb5e5bc19e989
Xah Lee <xah@xahlee.org>
2014-03-08 06:03:37 -0800

    for now, removed setting the menu/app key to call smex. Because ergoemacs-mode has its own setup. I haven't got the whole setup figured out… and ergoemacs distro may fail to have menu key do M-x… but will fix this later.

--- ergoemacs/init_load_packages.el
+++ ergoemacs/init_load_packages.el
@@ -79,27 +79,28 @@
 (require 'dired+)
 
 ;;; enhanced execute-extended-command
-;; make the menu key call smex's M-x. However, for some reason this doesn't work. Works only when this code is manually executed after emacs start.
 (require 'smex)
 (smex-initialize)
-(when (member 'smex features)
-  (cond
-   ((string-equal system-type "windows-nt")
-    (progn
-      (global-set-key (kbd "<apps>") 'smex)
-      (global-set-key (kbd "<S-apps>") 'smex-major-mode-commands)
-      )
-    )
-   ((string-equal system-type "darwin")
-    nil)
-   ((string-equal system-type "gnu/linux")
-    (progn
-      (global-set-key (kbd "<menu>") 'smex)
-      (global-set-key (kbd "<S-menu>") 'smex-major-mode-commands)
-      )
-    )
-   )
-  )
+
+;; ;; make the menu key call smex's M-x. However, for some reason this doesn't work. Works only when this code is manually executed after emacs start.
+;; (when (member 'smex features)
+;;   (cond
+;;    ((string-equal system-type "windows-nt")
+;;     (progn
+;;       (global-set-key (kbd "<apps>") 'smex)
+;;       (global-set-key (kbd "<S-apps>") 'smex-major-mode-commands)
+;;       )
+;;     )
+;;    ((string-equal system-type "darwin")
+;;     nil)
+;;    ((string-equal system-type "gnu/linux")
+;;     (progn
+;;       (global-set-key (kbd "<menu>") 'smex)
+;;       (global-set-key (kbd "<S-menu>") 'smex-major-mode-commands)
+;;       )
+;;     )
+;;    )
+;;   )
 
 ;; a user interface library used by auto-complete
 (add-to-list 'load-path
