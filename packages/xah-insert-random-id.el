b76c08d3acc7dc4923c649368076dfdb445e9dd6
Xah Lee <xah@xahlee.org>
2014-04-23 02:57:10 -0700

    renamed insert-random-id.el → xah-insert-random-id.el and also some  code improvement. —cowboy hacker sayz

--- packages/xah-insert-random-id.el
+++ packages/xah-insert-random-id.el
@@ -17,28 +17,27 @@
 ;; misc collection of commands to insert hex, digits, of fixed number of digits.
 
 ;;; Install:
-;; (require 'insert-random-id)
+;; (require 'xah-insert-random-id)
 
 ;;; Todo:
 ;; code needs to be refactored, so that there are a bunch of functions that returns a value, then one interactive command wrapper to them. Also, the command need to accept universal-argument for number of digits to insert.
 
 ;;; Change Log:
+;; • 0.2 2014-04-23 package name changed from insert-random-id.el to xah-insert-random-id.el. And some other minor updates.
 ;; • 0.1 2013-04-19 first version. Moved from my personal init.
-
 
 (random t) ; set seed
 
-(defun ξ-insert-random-hex ()
-  "Insert a random 4-digit hexidecimal number."
-  (interactive)
-  (let* ((myCharset "1234567890abcdef" )
+(defun xah-insert-random-hex (πcount)
+  "Insert πcount random hexidecimal digits.
+πcount default to 8"
+  (interactive "P")
+  (let* ((myCharset "0123456789abcdef" )
         (possibleCharsCount (length myCharset)))
-    (dotimes (ii 4)
-      (insert (elt myCharset (random possibleCharsCount))) ) )
-  ;; (insert (format "%4x" (random 65535)) )
-  )
+    (dotimes (ii (if (numberp πcount) (abs πcount) 8 ))
+      (insert (elt myCharset (random possibleCharsCount))) ) ))
 
-(defun ξ-insert-random-string ()
+(defun xah-insert-random-string ()
   "Insert a random alphanumerics string of length 5.
 The possible chars are 0 to 9, and a to z (lower case)."
   (interactive)
@@ -47,7 +46,7 @@ The possible chars are 0 to 9, and a to z (lower case)."
     (dotimes (ii 5)
       (insert (elt myCharset (random possibleCharsCount))) ) ) )
 
-(defun ξ-insert-random-uuid ()
+(defun xah-insert-random-uuid ()
   "Insert a random universally unique identifier (UUID).
 
 UUID is a 32 digits hexadecimal formatted in certain way with dash.
@@ -65,7 +64,7 @@ Example of a UUID: 1df63142-a513-c850-31a3-535fc3520c3d
            (random (expt 16 6)) ) ) )
 
 ;; primarily Christopher Wellons. 2011-11-18
-(defun ξ-insert-random-uuid-2 ()
+(defun xah-insert-random-uuid-2 ()
   "Insert a UUID. This uses a simple hashing of variable data."
   (interactive)
   (let ((myStr (md5 (format "%s%s%s%s%s%s%s%s%s%s"
@@ -87,7 +86,7 @@ Example of a UUID: 1df63142-a513-c850-31a3-535fc3520c3d
                     (substring myStr 17 20)
                     (substring myStr 20 32)))))
 
-(defun ξ-insert-random-number ()
+(defun xah-insert-random-number ()
   "Insert a random number of length 5."
   (interactive)
   (let (myCharset (possibleCharsCount 10))
@@ -95,7 +94,7 @@ Example of a UUID: 1df63142-a513-c850-31a3-535fc3520c3d
     (dotimes (ii 5)
       (insert (elt myCharset (random possibleCharsCount))) ) ) )
 
-(provide 'insert-random-id)
+(provide 'xah-insert-random-id)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; insert-random-id.el ends here
