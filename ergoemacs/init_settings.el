880b4d85986f2211cf316db1f4e18ac0be1fa158
Xah Lee <xah@xahlee.org>
2014-05-24 01:03:34 -0700

    changed to partial completion face color from red to green. Experimental.

--- ergoemacs/init_settings.el
+++ ergoemacs/init_settings.el
@@ -248,11 +248,10 @@ No splash screen. and If the *scratch* buffer is the current one, then create a 
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- ;; '(completions-common-part ((t (:inherit default :foreground "red"))))
  ;; '(diredp-ignored-file-name ((t (:foreground "#bebebe"))))
  ;; '(diredp-compressed-file-suffix ((t (:foreground "#7b68ee"))))
  ;; '(diredp-ignored-file-name ((t (:foreground "#aaaaaa"))))
  ;; '(isearch ((((class color) (min-colors 88) (background light)) (:background "black" :foreground "white"))))
+ '(completions-common-part ((t (:inherit default :foreground "green3"))))
  '(show-paren-match ((((class color) (background light)) (:background "azure2"))))
  )
-
