84e496e827cd75721bc75bb815dfb2f7981cdf3e
Xah Lee <xah@xahlee.org>
2014-06-02 04:19:29 -0700

    init_keybinding.el renamed browser-nav-keys to add-browser-backspace-key-to-Info-mode

--- ergoemacs/init_keybinding.el
+++ ergoemacs/init_keybinding.el
@@ -7,7 +7,7 @@
 
 (global-set-key (kbd "C-S-z") 'redo)
 
-(defun browser-nav-keys ()
+(defun add-browser-backspace-key-to-Info-mode ()
   "Add some browser styled navigation keys for `Info-mode'.
   The following keys are added:
  【Backspace】 for `Info-history-back'
@@ -19,7 +19,7 @@
   ;; note: on Linux Firefox, you have to turn on Backspace key for previous page. In the preference.
   )
 
-(add-hook 'Info-mode-hook 'browser-nav-keys)
+(add-hook 'Info-mode-hook 'add-browser-backspace-key-to-Info-mode)
 
 ;; In Windows, Alt+F4 closes the frame (or kill emacs if it is the last frame)
 (if (and (boundp 'w32-initialized) w32-initialized)
